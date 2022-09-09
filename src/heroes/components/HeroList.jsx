ec935fcd40b3b49e66deff6c3c89130de4f4e54b
Juan José Martínez <juanjmm@gmail.com>
2022-09-09 01:02:11 -0600

    Hero list was added

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useMemo } from 'react';
+import { getHeroesByPublishers } from '../helpers/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = useMemo(() => getHeroesByPublishers(publisher), [publisher]);
+
+    return (
+        <div className='row rows-cols-1 row-cols-md-2 g-3'>
+            {
+                heroes.map(hero => (
+                    <HeroCard key={hero.id}
+                        {...hero}
+                    />
+                ))
+            }
+        </div>
+    )
+}
